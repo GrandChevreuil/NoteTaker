ea2095045ac6a5fdd68bd842754afe33f902d544
GrandChevreuil <mathis.mauprivez@protonmail.com>
2025-10-06 10:07:27 +0200

    feat - data converters

---
+++ app/src/main/java/fr/eseo/ld/mm/notesroom/data/NoteTakerDataConverters.kt
@@ -0,0 +1,25 @@
+package fr.eseo.ld.mm.notesroom.data
+
+import androidx.room.TypeConverter
+import java.time.Instant
+import java.time.LocalDateTime
+import java.time.ZoneId
+
+class NoteTakerDataConverters {
+    @TypeConverter
+    fun fromTimeStamp(value: Long?): LocalDateTime? {
+        return value?.let {
+            Instant.ofEpochSecond(it)
+                .atZone(ZoneId.systemDefault())
+                .toLocalDateTime()
+        }
+    }
+
+    @TypeConverter
+    fun toTimeStamp(value: LocalDateTime?): Long? {
+        return value?.let {
+            it.atZone(ZoneId.systemDefault()).toEpochSecond()
+        }
+    }
+}
+
