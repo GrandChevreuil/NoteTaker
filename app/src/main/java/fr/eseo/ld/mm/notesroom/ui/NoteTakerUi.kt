1c2d05c65ee89f6df711b6f8d2432f29f62302be
GrandChevreuil <mathis.mauprivez@protonmail.com>
2025-10-06 10:36:13 +0200

    feat - notetakeUI modified to use the room based repository

--- app/src/main/java/fr/eseo/ld/mm/notesroom/ui/NoteTakerUi.kt
+++ app/src/main/java/fr/eseo/ld/mm/notesroom/ui/NoteTakerUi.kt
@@ -1,24 +1,28 @@
 package fr.eseo.ld.mm.notesroom.ui
 
-import fr.eseo.ld.mm.notesroom.ui.screens.*
+import android.app.Application
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavType
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
-import fr.eseo.ld.mm.notesroom.repositories.NoteTakerRepositoryListImpl
+import fr.eseo.ld.mm.notesroom.repositories.NoteTakerRepositoryRoomImpl
 import fr.eseo.ld.mm.notesroom.ui.navigation.NoteTakerScreens
+import fr.eseo.ld.mm.notesroom.ui.screens.DetailsScreen
+import fr.eseo.ld.mm.notesroom.ui.screens.SummaryScreen
 import fr.eseo.ld.mm.notesroom.ui.viewmodels.NoteTakerViewModel
 import fr.eseo.ld.mm.notesroom.ui.viewmodels.NoteTakerViewModelFactory
 
 @Composable
 fun NoteTakerUi() {
     val navController = rememberNavController()
+    val application = LocalContext.current.applicationContext as Application
     val viewModel : NoteTakerViewModel = viewModel(
         factory = NoteTakerViewModelFactory(
-            NoteTakerRepositoryListImpl()
+            NoteTakerRepositoryRoomImpl(application)
         )
     )
 
