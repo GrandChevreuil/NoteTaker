915b68022f6e612865083359af96d5b1829ebc52
GrandChevreuil <mathis.mauprivez@protonmail.com>
2025-10-06 11:17:22 +0200

    update -  build scripts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,6 +3,10 @@ plugins {
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.ksp)
+    alias(libs.plugins.gms)
+    alias(libs.plugins.firebase.perf)
+    alias(libs.plugins.firebase.crashlytics)
+    alias(libs.plugins.dagger.hilt.android)
 }
 
 android {
@@ -64,6 +68,20 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     ksp(libs.androidx.room.compiler)
     implementation(libs.androidx.room.ktx)
+    implementation(libs.androidx.hilt.navigation)
+    implementation(libs.androidx.preference)
+    implementation(libs.kotlinx.coroutines.play.services)
+    implementation(libs.hilt.android)
+    ksp(libs.hilt.ksp)
+    implementation(platform(libs.firebase.bom))
+    implementation(libs.firebase.analytics)
+    implementation(libs.firebase.crashytics)
+    implementation(libs.firebase.auth)
+    implementation(libs.firebase.firestore)
+    implementation(libs.firebase.perf)
+    implementation(libs.firebase.config)
+    implementation(libs.firebase.messaging)
+    implementation(libs.androidx.lifecycle.livedata.ktx)
 
 }
 
