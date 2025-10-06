3468f7daa754e8830bfed6f89b2c7170e9e322c6
GrandChevreuil <mathis.mauprivez@protonmail.com>
2025-10-06 11:23:36 +0200

    feat - Hilt Module to provide for certain instances

--- app/src/main/java/fr/eseo/ld/mm/notescloud/MainActivity.kt
+++ app/src/main/java/fr/eseo/ld/mm/notescloud/MainActivity.kt
@@ -5,9 +5,11 @@ import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import dagger.hilt.android.AndroidEntryPoint
 import fr.eseo.ld.mm.notescloud.ui.NoteTakerUi
 import fr.eseo.ld.mm.notescloud.ui.theme.NoteTakerTheme
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
---
+++ app/src/main/java/fr/eseo/ld/mm/notescloud/hiltmodules/FirebaseModule.kt
@@ -0,0 +1,17 @@
+package fr.eseo.ld.mm.notescloud.hiltmodules
+
+import com.google.firebase.auth.FirebaseAuth
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FirebaseModule {
+    @Provides
+    @Singleton
+    fun provideFirebaseAuthentication(): FirebaseAuth = FirebaseAuth.getInstance()
+}
+
